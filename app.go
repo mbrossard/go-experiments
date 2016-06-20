b57c6d61825a94d4ef69928dce28182a5ff63ab0
Mathias Brossard <mathias@brossard.org>
2016-06-19 23:22:01 -0700

    Simple Hello World

---
+++ app.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world!")
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
