c46169318119ab787f55a1e13a646652e5db2057
Mathias Brossard <mathias@brossard.org>
2016-06-27 00:19:26 -0700

    Use menu instead of header

--- app/layout/Layout.jsx
+++ app/layout/Layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import Header from './Header.jsx'
+import Menu from './Menu.jsx'
 let Layout = React.createClass({
     render: function(){
         return (
             <div>
-                <Header />
+                <Menu />
 
                 <div id="main" role="main">
                     {this.props.children}
