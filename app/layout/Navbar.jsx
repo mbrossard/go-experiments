ed5280ecba399e34f1743e219cd414a176d50d49
Mathias Brossard <mathias@brossard.org>
2016-07-02 01:13:54 -0700

    New navigation bar

---
+++ app/layout/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+let Navbar = React.createClass({
+    render: function () {
+        return <nav className="navbar navbar-default navbar-static-top" role="navigation">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="/">
+                <i className="fa fa-lg fa-rocket" aria-hidden="true"></i>
+              </a>
+            </div>
+
+            <ul className="nav navbar-top-links navbar-right">
+            </ul>
+            </nav>;
+    }
+});
+
+export default Navbar
+
