4736c4b55b06f55e6dd1ff55d07598dedd520f73
Mathias Brossard <mathias@brossard.org>
2016-07-03 23:22:37 -0700

    Add navigation bar to layout

--- app/layout/Layout.jsx
+++ app/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import Navbar from './Navbar.jsx'
 import Header from './Header.jsx'
 import Menu from './Menu.jsx'
 let Layout = React.createClass({
     render: function(){
         return (
             <div>
-                <Menu />
+                <Navbar />
+                <Menu />                
 
                 <div id="main" role="main">
                     {this.props.children}
